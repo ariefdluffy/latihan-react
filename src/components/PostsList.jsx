58f4360b43d233808f77d4c9b2b198dcb2207037
arieftheluffy <arieftheluffy@gmail.com>
2024-12-03 06:14:36 +0800

    coba-rest-api-post

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -10,6 +10,13 @@ function PostsList({ isPosting, onStopPosting }) {
     const [posts, setPosts] = useState([])
 
     function addPostHandler(postData) {
+        fetch('http://localhost:8080/posts', {
+            method: 'POST',
+            body: JSON.stringify(postData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
         setPosts((existingPosts) => [postData, ...existingPosts])
     }
 
@@ -23,10 +30,17 @@ function PostsList({ isPosting, onStopPosting }) {
                     />
                 </Modal>
             )}
-
-            <ul className={classes.posts}>
-                {posts.map((post) => <Post author={post.author} body={post.body} />)}
-            </ul>
+            {posts.length > 0 && (
+                <ul className={classes.posts}>
+                    {posts.map((post) => <Post key={post.body} author={post.author} body={post.body} />)}
+                </ul>
+            )}
+            {posts.length === 0 && (
+                <div style={{ textAlign: 'center', color: 'white' }}>
+                    <h2>Belum ada post...</h2>
+                    <p>Tambahkan post</p>
+                </div>
+            )}
         </>
     )
 }
